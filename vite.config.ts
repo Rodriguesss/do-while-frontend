0a30d5dbf5faff4c7235e2dab08556792128d039
rodriguesss <viniciusrdefreitas@gmail.com>
2022-03-16 16:57:20 -0300

    configured env prod

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import * as dotenv from "dotenv";
+
+dotenv.config({ path: __dirname + '/.env' });
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 5000,
+    port: parseInt(process.env.PORT),
+    https: true,
     cors: true
+  },
+  build: {
+    minify: true
   },
   plugins: [react()]
 })
